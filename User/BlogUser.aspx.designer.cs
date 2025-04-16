49eed193b43fed3eb03b636d337af6ec5e75d89f
Asha Kotak <akotak726@rku.ac.in>
2025-04-16 13:45:29 +0530

    BlogUser change

--- User/BlogUser.aspx.designer.cs
+++ User/BlogUser.aspx.designer.cs
@@ -13,14 +13,5 @@ namespace Aesthetica_design.User
 
     public partial class BlogUser
     {
-
-        /// <summary>
-        /// form1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
     }
 }
